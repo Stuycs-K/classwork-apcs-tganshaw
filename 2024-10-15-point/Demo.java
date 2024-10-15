211778eade88ba5dcbc8b00325dc3037d93c90d8
tganshaw <tudorganshaw@gmail.com>
2024-10-15 18:43:21 -0400

    Added triangle points

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -18,7 +18,7 @@ public class Demo{
     System.out.println( p3+ " with expected value of: " + "(" + p3.getX() + ",  " + p3.getY() + ")");
     System.out.println( distance(p1,p2) + " with expected value of: " + Math.sqrt(8));
     System.out.println( Point.distance(p1,p2)+ " with expected value of: " + Math.sqrt(8));
-    System.out.println( p1.distanceTo(p2)+ " with expected value of: " + Math.sqrt(8));}
+    System.out.println( p1.distanceTo(p2)+ " with expected value of: " + Math.sqrt(8));
 
     Point trione = new Point(0.0,0.0);
     Point tritwo = new Point(3.0,0.0);
@@ -26,4 +26,5 @@ public class Demo{
     System.out.println(Point.distance(trione, tritwo));
     System.out.println(Point.distance(tritwo, trithree ));
     System.out.println(Point.distance(trithree, trione ));
+  }
 }
