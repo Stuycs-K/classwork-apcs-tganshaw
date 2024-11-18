22f4859240acfc48e48abb3ba45f509fe17d0329
tganshaw <tudorganshaw@gmail.com>
2024-11-18 18:15:53 -0500

    did day 2 part 2 for the fun of it

--- 2024-11-18-advent2016/day2part1.java
+++ 2024-11-18-advent2016/day2part1.java
@@ -8,7 +8,7 @@ public class day2part1{
 public static void main(String[]args){
 
 System.out.println("Combination is " + keypadCombo("inputday2.txt"));
-
+System.out.println("Combination 2 is " + keypadCombo2("inputday2.txt"));
 }
 
 
@@ -69,10 +69,85 @@ for(int i = 0; i < arr.length; i++)
 return combo;
 }
 
+public static String keypadCombo2(String filename){
+
+String combo = "";
+char[][] keypad = {
+  {'0','0','1','0','0'},
+  {'0','2','3','4','0'},
+  {'5','6','7','8','9'},
+  {'0','A','B','C','0'},
+  {'0','0','D','0','0'}};
+int ypos = 2;
+int xpos = 0;
+
+try{
+
+File file = new File(filename);
+Scanner sc = new Scanner(file);
+
+while(sc.hasNextLine()){
+String[] arr = sc.nextLine().strip().split("\n");
+
+for(int i = 0; i < arr.length; i++)
+{
+
+  for(int n = 0; n < arr[i].length(); n++)
+  {
+    if(arr[i].charAt(n) == 'U' && trackerchecker(keypad[ypos][xpos],"U"))
+    {
+      ypos--;
+    }
+    if(arr[i].charAt(n) == 'D' && trackerchecker(keypad[ypos][xpos],"D"))
+    {
+      ypos++;
+    }
+    if(arr[i].charAt(n) == 'L' & trackerchecker(keypad[ypos][xpos],"L"))
+    {
+      xpos--;
+    }
+    if(arr[i].charAt(n) == 'R' && trackerchecker(keypad[ypos][xpos],"R"))
+    {
+      xpos++;
+    }
+
+    System.out.println(xpos + " " + ypos);
 
 
+  }
+  combo += keypad[ypos][xpos];
+}
+}
 
 
+}catch(FileNotFoundException e) {System.out.println("notfiled");}
+
+
+return combo;
+}
+
+
+public static boolean trackerchecker(char char1, String direction)
+{
+  if(direction == "U")
+  {
+    if(char1 == '1' || char1 == '2' || char1 == '4' || char1 =='5' || char1 == '9')
+    {return false;}
+  }
+  if (direction == "D"){
+    if(char1 == 'D' || char1 == 'A' || char1 == 'C' || char1 =='5' || char1 == '9')
+    {return false;}
+  }
+  if (direction == "L"){
+    if(char1 == 'D' || char1 == 'A' || char1 == '1' || char1 =='5' || char1 == '2')
+    {return false;}
+  }
+  if (direction == "R"){
+    if(char1 == 'D' || char1 == 'C' || char1 == '1' || char1 =='9' || char1 == '4')
+    {return false;}
+  }
+  return true;
+}
 
 
 }
