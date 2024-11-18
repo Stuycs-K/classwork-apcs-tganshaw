8552534c704dab831e4f4f5c9d0c3d35891e92bb
tganshaw <tudorganshaw@gmail.com>
2024-11-18 18:25:17 -0500

    Tried to start on day 1 part 2, did not get far

--- 2024-11-18-advent2016/day1part1.java
+++ 2024-11-18-advent2016/day1part1.java
@@ -68,7 +68,57 @@ System.out.println(hori + " " + vert);
 return Math.abs(vert) + Math.abs(hori);
 }
 
+public static int walkingDistance2(String filename){
 
+  int distance = 0;
+  int angle = 0;
+  int vert = 0;
+  int hori = 0;
+
+  try{
+    File file = new File(filename);
+    Scanner sc = new Scanner(file);
+
+    String[] arr = sc.nextLine().split(", ");
+
+    for(int i = 0; i < arr.length; i++)
+    {
+      System.out.print(arr[i] + " ");
+      if(arr[i].charAt(0) == 'R')
+      {
+        angle -= 90;
+
+      }
+      if(arr[i].charAt(0) == 'L')
+      {
+        angle += 90;
+      }
+      if(angle < 0){angle += 360;}
+      if(angle >= 360){angle -= 360;}
+      System.out.println(angle);
+      if(angle == 0)
+      {
+        vert += Integer.parseInt(arr[i].substring(1));
+      }
+      if(angle == 180)
+      {
+        vert -= Integer.parseInt(arr[i].substring(1));
+      }
+      if(angle == 90)
+      {
+        hori += Integer.parseInt(arr[i].substring(1));
+      }
+      if(angle == 270)
+      {
+        hori -= Integer.parseInt(arr[i].substring(1));
+      }
+
+    }
+  }catch(FileNotFoundException e) {System.out.println("nofile");}
+
+  System.out.println(hori + " " + vert);
+  return Math.abs(vert) + Math.abs(hori);
+}
 
 
 }
