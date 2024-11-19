7d9fe8d8593d2b8373986f0a56e4ca925c915ddc
Tudor Ganshaw <tudorganshaw@gmail.com>
2024-11-19 10:12:56 -0500

    Remember to check out 2DArray next time you do this

--- 2024-11-18-advent2016/day4.java
+++ 2024-11-18-advent2016/day4.java
@@ -16,47 +16,68 @@ int sum = 0;
   try{
     File file = new File(filename);
     Scanner sc = new Scanner(file);
-    int[] alphabetarr = new int[26];
+
 
 while(sc.hasNextLine()){
     String[] arr = sc.nextLine().split("-");
+        int[] alphabetarr = new int[26];
+        System.out.println("------");
 
-    for(int i = 0; i < arr.length;i++)
-    {
-    //  System.out.println(arr[i]);
-    }
     boolean checkedsum = true;
 
     for(int i =0; i < arr.length;i++)
     {
       String checksum = arr[arr.length-1];
+      System.out.println(checksum);
       System.out.println(arr[i]);
-      String checksum2 =checksum.substring(4,10);
+      String checksum2 =checksum.substring(4,9);
+      System.out.println(checksum2);
       for(int n = 0; n < arr[i].length(); n++)
       {
 
       if(i < arr.length-1){alphabetarr[(int)arr[i].charAt(n)-97]++;}
       }
       if(i == arr.length-1){
+
       for(int x = 0; x < checksum2.length()-1;x++)
       {
-        if(alphabetarr[(int)checksum2.charAt(x)-97] == 0)
+      /*  System.out.println(checksum2.charAt(x));
+        System.out.println(checksum2.charAt(x+1));
+      */  System.out.println(checksum2);
+
+        System.out.println(alphabetarr[((int)checksum2.charAt(x))-97]);
+        System.out.println(alphabetarr[((int)checksum2.charAt(x+1))-97]);
+
+        char chari = checksum2.charAt(x);
+        char chari2 = checksum2.charAt(x+1);
+        int int1 = alphabetarr[((int)checksum2.charAt(x))-97];
+        int int2 = alphabetarr[((int)checksum2.charAt(x+1))-97];
+        System.out.println(chari);
+        System.out.println(chari2);
+
+        if(int1 == 0 || int2 == 0)
         {
           checkedsum = false;
-          //System.out.println(checkedsum);
         }
-        else if((alphabetarr[(int)checksum2.charAt(x)-97] < alphabetarr[(int)checksum2.charAt(x+1)-97]))
+        if(int2 > int1)
         {
           checkedsum = false;
         }
-        else if ((alphabetarr[(int)checksum2.charAt(x)-97] == alphabetarr[(int)checksum2.charAt(x+1)-97]) && checksum2.charAt(x) > checksum2.charAt(x+1)   )
+        if(int1 == int2)
         {
-          checkedsum = false;
+          if(chari > chari2)
+          {
+            checkedsum = false;
+          }
         }
+
+
       }
       if(checkedsum)
-      {sum+=Integer.parseInt(checksum.substring(0,3));
-      System.out.println(checkedsum);}
+      {
+        System.out.println(true);
+        sum+=Integer.parseInt(checksum.substring(0,3));
+      }
       //System.out.println(Integer.parseInt(checksum.substring(0,3)));
     }
 
