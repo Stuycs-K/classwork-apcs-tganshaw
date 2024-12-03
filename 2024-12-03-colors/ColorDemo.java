86bf3e050ef040a5d14c1648288ecf4afd1c369c
Tudor Ganshaw <tudorganshaw@gmail.com>
2024-12-03 10:04:18 -0500

    Added some color testing

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,15 @@
+public class ColorDemo{
+
+public static void main(String[] args) {
+
+  System.out.println("\u001b[36mCyan");
+  System.out.println("\u001b[43mBackgroundYellow");
+  System.out.println("\u001b[H");
+    System.out.println("\u001b[36mCyanAgain");
+
+  System.out.println("\n\u001b[39;49mReset");
+}
+
+
+
+}
