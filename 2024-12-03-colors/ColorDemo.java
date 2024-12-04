702d1175128c0e93062dc4e0fd1520c1f2bc79f2
tganshaw <tudorganshaw@gmail.com>
2024-12-03 19:33:04 -0500

    Updated ColorDemo to be more interesting

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -2,12 +2,24 @@ public class ColorDemo{
 
 public static void main(String[] args) {
 
+
+  System.out.println("\u001b[2J");
   System.out.println("\u001b[36mCyan");
   System.out.println("\u001b[43mBackgroundYellow");
   System.out.println("\u001b[H");
-    System.out.println("\u001b[36mCyanAgain");
+  System.out.print("\u001b[36mCyanAgain");
+  System.out.println("\u001b[1;1f");
+  System.out.println("\n\u001b[mReset");
+  System.out.println("\u001b[38;2;140;93;72;7mDifferentColor");
+  System.out.println("\n\u001b[mReset");
 
-  System.out.println("\n\u001b[39;49mReset");
+  for(int i = 0; i < 17; i++)
+  {
+    int color = 30+i;
+    if(color%10 > 7){color = ((color % 10) + 1) * 10;}
+    System.out.println("\u001b[" + color + "mColor");
+  }
+  System.out.println("\n\u001b[mReset");
 }
 
 
