2792fdfb102df34065a602ef7bf157bd6c91f6db
Tudor Ganshaw <tudorganshaw@gmail.com>
2025-01-03 10:14:00 -0500

    Finished makeWords

---
+++ 2025-01-02-Fibonacci/makeWords.java
@@ -0,0 +1,46 @@
+public class makeWords{
+
+
+
+
+public static void main(String[] args) {
+  makeWords(2,"","abc");
+}
+
+public static void makeWords(int remainingLetters, String result, String alphabet){
+
+
+if(remainingLetters == 0)
+{
+  for(int i = 0; i < alphabet.length();i++)
+  {
+  System.out.println(result + alphabet.charAt(i));
+  }
+}
+else
+{
+if(remainingLetters == 1)
+{
+
+  for(int i = 0; i < alphabet.length();i++){
+
+  makeWords(remainingLetters-1,result+alphabet.charAt(i),alphabet);}
+
+}
+else
+{
+  for(int i = 0; i < alphabet.length();i++)
+  {
+    makeWords(remainingLetters-1,result + alphabet.charAt(i),alphabet);
+  }
+}
+
+
+}
+
+}
+
+
+
+
+}
